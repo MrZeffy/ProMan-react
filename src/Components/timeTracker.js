45ade11b12e5b1c443309c72b30f4153243d274f
ishika12369 <ishika1236>
2021-06-23 16:58:39 +0530

    started working on timetracker

---
+++ src/Components/timeTracker.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './timeTracker.css'
+
+
+/*const projects = {
+    allProjects: []
+};
+class Project {
+    constructor(id, title) {
+      this.id = id;
+      this.title = title;
+    }
+  }
+
+const newProject = new Project(ID, title);
+projects.allProjects.push(newProject);*/
+
+
+
+const timeTracker = () => {
+    return (
+        <>
+
+            <form>
+                <input placeholder="what are you working on"></input>
+                <input type="submit" value="Submit"></input>
+            </form>
+
+
+            <ul className="projectslist">
+            </ul>
+
+        </>
+    )
+}
+
+export default timeTracker
